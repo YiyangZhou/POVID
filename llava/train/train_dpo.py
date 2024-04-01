5d55ce605230f5ad3889701a894a98ddca6e1534
Yiyang Zhou <74579698+YiyangZhou@users.noreply.github.com>
2024-04-01 16:27:55 +0800

    Update train_dpo.py

--- llava/train/train_dpo.py
+++ llava/train/train_dpo.py
@@ -94,6 +94,7 @@ class ModelArguments:
     mm_use_im_start_end: bool = field(default=False)
     mm_use_im_patch_token: bool = field(default=True)
     mm_vision_select_feature: Optional[str] = field(default="patch")
+    mm_patch_merge_type: Optional[str] = field(default='flat')
 
 
 @dataclass
